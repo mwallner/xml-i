6e06ea2d9ac3646b8e717e16fdee1eabef8adbac
Manfred Wallner <schusterfredl@mwallner.net>
2026-03-12 21:55:43 +0100

    update .net8 to .net10

--- alien/NET/NET.bc.ps1
+++ alien/NET/NET.bc.ps1
@@ -2,13 +2,20 @@
 # will be dot-sourced by xml-i.build.ps1
 
 $decl = @{
-	Name        = '.NET 8 - XmlReader' 
+	Name        = '.NET 10 - XmlReader' 
 	Description = 'xml-i in .NET'
 	ParserType  = 'StAX'
 	Origin      = $PSScriptRoot
 	Meta        = {	dotnet --version }
-	Builder     = {	
-		& dotnet publish 'src' -c Release -o '../bin/dotnet'
+	Builder     = {    
+		Push-Location 'src'
+		try {
+			& dotnet publish -c Release
+			Copy-Item -Path 'bin/Release/net10.0/publish/xml-i-dotnet' -Destination '../../bin/dotnet/xml-i-dotnet' -Force
+		}
+		finally {
+			Pop-Location
+		}
 	}
 	Tester      = @{
 		Executable   = '../bin/dotnet/xml-i-dotnet'
