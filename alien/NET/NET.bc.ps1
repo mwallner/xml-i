f3c52509cfac73e4fe84a8d3a94e6ad7c68463bd
Manfred Wallner <schusterfredl@mwallner.net>
2026-03-13 20:55:52 +0100

    keep net8 alongside net10

--- alien/NET/NET.bc.ps1
+++ alien/NET/NET.bc.ps1
@@ -1,25 +1,50 @@
 
 # will be dot-sourced by xml-i.build.ps1
 
-$decl = @{
-	Name        = '.NET 10 - XmlReader' 
-	Description = 'xml-i in .NET'
+# .NET 10 build declaration
+$decl10 = @{
+	Name        = '.NET 10 - XmlReader'
+	Description = 'xml-i in .NET 10'
 	ParserType  = 'StAX'
 	Origin      = $PSScriptRoot
-	Meta        = {	dotnet --version }
-	Builder     = {    
+	Meta        = { dotnet --version }
+	Builder     = {
 		Push-Location 'src'
 		try {
-			& dotnet publish -c Release
-			Copy-Item -Path 'bin/Release/net10.0/publish/xml-i-dotnet' -Destination '../../bin/dotnet/xml-i-dotnet' -Force
+			& dotnet publish -c Release 'xml-i-dotnet-10.csproj'
+			Copy-Item -Path 'bin/Release/net10.0/publish/xml-i-dotnet' -Destination '../../bin/dotnet/xml-i-dotnet-10' -Force
 		}
 		finally {
 			Pop-Location
 		}
 	}
 	Tester      = @{
-		Executable   = '../bin/dotnet/xml-i-dotnet'
-		ArgumentList = @() 
- }
+		Executable   = '../bin/dotnet/xml-i-dotnet-10'
+		ArgumentList = @()
+	}
 }
-New-AppDecl @decl
+New-AppDecl @decl10
+
+# .NET 8 build declaration
+$decl8 = @{
+	Name        = '.NET 8 - XmlReader'
+	Description = 'xml-i in .NET 8'
+	ParserType  = 'StAX'
+	Origin      = $PSScriptRoot
+	Meta        = { dotnet --version }
+	Builder     = {
+		Push-Location 'src'
+		try {
+			& dotnet publish -c Release 'xml-i-dotnet-8.csproj'
+			Copy-Item -Path 'bin/Release/net8.0/xml-i-dotnet' -Destination '../../bin/dotnet/xml-i-dotnet-8' -Force
+		}
+		finally {
+			Pop-Location
+		}
+	}
+	Tester      = @{
+		Executable   = '../bin/dotnet/xml-i-dotnet-8'
+		ArgumentList = @()
+	}
+}
+New-AppDecl @decl8
